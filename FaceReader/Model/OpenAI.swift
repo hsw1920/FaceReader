5ff9d3a740addf9d9debc7fa27949515870ad530
hsw1920 <hsw1920@gmail.com>
2023-11-11 21:11:09 +0900

    [Feat] OpenAI API의 Response Codable Model 추가

---
+++ FaceReader/Model/OpenAI.swift
@@ -0,0 +1,30 @@
+//
+//  OpenAI.swift
+//  FaceReader
+//
+//  Created by 홍승완 on 2023/11/11.
+//
+
+import Foundation
+
+struct OpenAIResponse: Codable {
+    var id: String?
+    var object: String?
+    var created: Int?
+    var choices: [Choice]
+    var usage: Usage?
+}
+
+struct Choice: Codable {
+    var index: Int?
+    var message: [String: String]
+    var finish_reason: String?
+}
+
+
+struct Usage: Codable {
+    var prompt_tokens: Int?
+    var completion_tokens: Int?
+    var total_tokens: Int?
+}
+
