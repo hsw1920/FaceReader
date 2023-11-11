110d4bccce65d48cfc8c4c175d87579b0b736732
hsw1920 <hsw1920@gmail.com>
2023-11-11 21:01:26 +0900

    Initial commit

---
+++ FaceReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FaceReader
+//
+//  Created by 홍승완 on 2023/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
