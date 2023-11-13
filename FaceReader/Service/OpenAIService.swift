f11652d32abff3d5fd4a6fa70f5fd7187f7b4c3f
hsw1920 <hsw1920@gmail.com>
2023-11-13 10:40:12 +0900

    chore

--- FaceReader/Service/OpenAIService.swift
+++ FaceReader/Service/OpenAIService.swift
@@ -8,45 +8,9 @@
 import UIKit
 import RxSwift
 
-func apiCall(name: String) {
-    let openAIKey = "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"
-    guard let url = URL(string: "https://api.openai.com/v1/chat/completions") else { return }
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-    request.addValue("Bearer \(openAIKey)", forHTTPHeaderField: "Authorization")
-    
-    let httpBody: [String: Any] = [
-        "model" : "gpt-3.5-turbo",
-        "messages" : [
-            ["role": "system",
-             "content": "한국 연예인 \(name)의 관상에 대해서 눈, 코, 입에 대해 각 1줄씩 총 3줄 관상을 봐줘 이때 \(name)이라는 단어는 \"당신\" 으로 치환해줘"]
-        ],
-        "temperature": 0.7
-    ]
-    
-    request.httpBody = try? JSONSerialization.data(withJSONObject: httpBody, options: .fragmentsAllowed)
-    
-    // URLSession을 사용하여 요청 전송
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-        guard let data = data, error == nil else {
-            print("error")
-            return
-        }
-        do {
-            let response = try JSONDecoder().decode(OpenAIResponse.self, from: data)
-            print("SUCCESS: \(response.choices[0].message.content)")
-        } catch {
-            print(error)
-        }
-    }
-    
-    task.resume()
-}
-
 func openAIRequest(name: String) -> Observable<String> {
     return Observable.create { observer in
-        let openAIKey = "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"
+        let openAIKey = Bundle.main.OPENAI_API_KEY
         guard let url = URL(string: "https://api.openai.com/v1/chat/completions") else {
             observer.onError(NSError(domain: "Invalid URL", code: 0, userInfo: nil))
             return Disposables.create()
@@ -56,7 +20,6 @@ func openAIRequest(name: String) -> Observable<String> {
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("Bearer \(openAIKey)", forHTTPHeaderField: "Authorization")
-        
         let httpBody: [String: Any] = [
             "model": "gpt-3.5-turbo",
             "messages": [
@@ -96,5 +59,21 @@ func openAIRequest(name: String) -> Observable<String> {
         return Disposables.create {
             // Cleanup code if needed
         }
+    }
+}
+
+extension Bundle {
+    
+    var OPENAI_API_KEY: String {
+        guard let file = self.path(forResource: "Info", ofType: "plist") else { return "" }
+        
+        // .plist를 딕셔너리로 받아오기
+        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+        
+        // 딕셔너리에서 값 찾기
+        guard let key = resource["OPENAI_API_KEY"] as? String else {
+            fatalError("OPENAI_API_KEY error")
+        }
+        return key
     }
 }
